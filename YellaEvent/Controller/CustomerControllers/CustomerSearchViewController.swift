7ae9237911a1214d64f35dca2ecd2ab941224733
dbsaw <hussain.sabbai@gmail.com>
2024-12-03 10:00:28 +0300

    added the seach screen

--- YellaEvent/Controller/CustomerControllers/CustomerSearchViewController.swift
+++ YellaEvent/Controller/CustomerControllers/CustomerSearchViewController.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class CustomerSearchViewController: UIViewController {
-
+  
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let prevSearch = ["Eminim", "AWS", "Fun", "GDG", "Swift"]
+    var filteredSearch: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        filteredSearch = prevSearch
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+
+extension CustomerSearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        filteredSearch.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = filteredSearch[indexPath.row]
+        return cell
     }
-    */
+}
 
+extension CustomerSearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" { filteredSearch = prevSearch} else {
+            filteredSearch = prevSearch.filter { item in item.lowercased().contains(searchText.lowercased())
+                    }
+        }
+        
+        tableView.reloadData()
+    }
 }
