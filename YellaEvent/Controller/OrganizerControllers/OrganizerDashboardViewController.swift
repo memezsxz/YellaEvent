74b86acde34644462113859ed14fa28b44da6a61
meme <mmeme0100@gmail.com>
2024-12-09 23:23:03 +0300

    update with removed errors

--- YellaEvent/Controller/OrganizerControllers/OrganizerDashboardViewController.swift
+++ YellaEvent/Controller/OrganizerControllers/OrganizerDashboardViewController.swift
@@ -23,33 +23,33 @@ class OrganizerDashboardViewController: UITableViewController {
     var organizer : Organizer?
     
     override func viewWillAppear(_ animated: Bool) {
-        Task {
-            try await EventsManager.getDashboardStats(organizerID: "212``2", completion: { canceledEvents,overEvents,ongoingEvents,allEvents,numTotalAttendence,revenue,averageRating,soldTickets in
-                
-                DispatchQueue.main.async {
-                    self.canceledEventsLabel.text = "\(canceledEvents)"
-                    self.overEventsLabel.text = "\(overEvents)"
-                    self.ongoingEventsLabel.text = "\(ongoingEvents)"
-                    self.allEventsLabel.text = "\(allEvents)"
-                    
-                    self.totalAttendence.text = "\(numTotalAttendence)"
-                    self.revenueLabel.text = String(format: "%.3f", revenue)
-                    self.averageRatingLabel.text = String(format: "%.1f", revenue)
-                    self.soldTicketsLabel.text = "\(soldTickets)"
-                    
-                }
-            }
-            )
-        }
+//        Task {
+//            try await EventsManager.getDashboardStats(organizerID: "212``2", completion: { canceledEvents,overEvents,ongoingEvents,allEvents,numTotalAttendence,revenue,averageRating,soldTickets in
+//                
+//                DispatchQueue.main.async {
+//                    self.canceledEventsLabel.text = "\(canceledEvents)"
+//                    self.overEventsLabel.text = "\(overEvents)"
+//                    self.ongoingEventsLabel.text = "\(ongoingEvents)"
+//                    self.allEventsLabel.text = "\(allEvents)"
+//                    
+//                    self.totalAttendence.text = "\(numTotalAttendence)"
+//                    self.revenueLabel.text = String(format: "%.3f", revenue)
+//                    self.averageRatingLabel.text = String(format: "%.1f", revenue)
+//                    self.soldTicketsLabel.text = "\(soldTickets)"
+//                    
+//                }
+//            }
+//            )
+//        }
         
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Task {
-            self.organizer = try await UsersManager.getOrganizer(organizerID: UserDefaults.standard.string(forKey: K.bundleUserID)!)
-        }
+//        Task {
+//            self.organizer = try await UsersManager.getOrganizer(organizerID: UserDefaults.standard.string(forKey: K.bundleUserID)!)
+//        }
         // Do any additional setup after loading the view.
     }
     
